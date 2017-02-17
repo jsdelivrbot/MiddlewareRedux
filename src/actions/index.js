5f746952a4bb564cf42933a7349d2964d2fd87de
Clint Ast <clintast@gmail.com>
2017-02-17 10:30:06 -0600

    made an action type const

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,2 +1,9 @@
 
 const API_KEY = '42a0ed0c49a0092f17154897c6c04bcb';
+export const FETCH_WEATHER = 'FETCH_WEATHER';
+
+export function fetchWeather() {
+  return {
+    type: FETCH_WEATHER
+  };
+}
