2f5aaf24f03d031d1613f2e7ef0a68cdaa45b405
Clint Ast <clintast@gmail.com>
2017-02-17 11:26:48 -0600

    added the fetch to search bar

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,3 +1,4 @@
+import axios from 'axios';
 const API_KEY = '42a0ed0c49a0092f17154897c6c04bcb';
 const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
 
@@ -7,7 +8,7 @@ export function fetchWeather(city) {
   const url = `${ROOT_URL}&q=${city},us`
   const request = axios.get(url);
   return {
-    type: FETCH_WEATHER
+    type: FETCH_WEATHER,
     payload: request
   };
 }
