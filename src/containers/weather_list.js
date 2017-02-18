267cf34d0eb16c0e38f01539516d45ca360a7b89
Clint Ast <clintast@gmail.com>
2017-02-18 09:35:49 -0600

    added weather to State

--- src/containers/weather_list.js
+++ src/containers/weather_list.js
@@ -1,18 +1,34 @@
 import React, {Component} from 'react';
+import {connect} from 'react-redux';
 
-export default class WeatherList extends Component {
+class WeatherList extends Component {
+  renderWeather(cityData) {
+    
+  }
+
   render() {
     return (
       <table className="table table-hover">
         <thead>
           <tr>
             <th>City</th>
+            <th>Temperature</th>
+            <th>Pressure</th>
+            <th>Humidity</th>
           </tr>
         </thead>
         <tbody>
-
+          {this.props.weather.map(this.renderWeather)}
         </tbody>
       </table>
     );
   }
 }
+
+// mapStateToProps(state)
+// return {weather = state.weather} just pull weather off so:
+// return {weather: weather}
+function mapStateToProps({weather}) {
+  return {weather};  // {weather} === {weather: weather}
+}
+export default connect(mapStateToProps)(WeatherList);
